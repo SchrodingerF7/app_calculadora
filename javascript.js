c32f908a789d2f0f78742f3facc1aea3952ed7d3
Fernando França <fernandofrancaf7@gmail.com>
2020-11-01 02:08:10 -0300

    inicio

---
+++ javascript.js
@@ -0,0 +1,41 @@
+
+function calcular (tipo, valor) {
+
+    if (tipo === 'acao') {
+
+
+    	if (valor === 'c') {
+
+    		document.getElementById('resultado').value = '';
+
+    	}
+
+    	if (valor === '/' || valor === '*' || valor === '-' || valor === '+' || valor === '.') {
+
+    		document.getElementById('resultado').value += valor;
+    		
+
+    	} 
+
+ 		if (valor === '=') {
+
+ 			var igual_campo = eval(document.getElementById('resultado').value);
+
+    		document.getElementById('resultado').value = igual_campo;
+
+ 		}
+
+
+    }
+
+    else if (tipo === 'valor') {
+
+    	var valor_campo = document.getElementById('resultado').value;
+
+    	document.getElementById('resultado').value = valor_campo + valor;
+
+    	/* document.getElementById('resultado').value += valor; */
+
+    }
+
+}
